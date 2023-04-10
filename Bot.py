05df246188565d4e897a78cdc5243f8ee9834d5d
markpereverzov <mark.pereverzov@gmail.com>
2023-04-10 23:26:55 +0300

    New command: deposit

--- Bot.py
+++ Bot.py
@@ -19,34 +19,44 @@ async def on_member_join(member):
 	else:
 		pass
 
-async def false_member(ctx, value):
-	emb = discord.Embed(title = ' ', color = 0xffb7fb)
+async def false_message(ctx, value):
+	emb = discord.Embed(title = '', color = 0xFF7575)
 	emb.set_author(name = ctx.author.name, icon_url = ctx.author.avatar)
-	emb.add_field(name = '', value = f':heavy_multiplication_x:  {value}.')
+	emb.add_field(name = f'**{value}.**', value = '')
 	emb.set_image(url = "https://cdn.discordapp.com/attachments/1093147291327668335/1093250649338167296/unknown_11.png")
 	await ctx.send(embed = emb)
 	await ctx.message.delete()
 
-async def true_member(ctx, value1, member, cash):
-	emb = discord.Embed(title = f'{value1}', color = 0xffb7fb)
+async def true_message(ctx, value1, member, cash):
+	emb = discord.Embed(title = f'{value1}', color = 0xB9FFA8)
 	emb.set_author(name = ctx.author.name, icon_url = ctx.author.avatar)
-	emb.add_field(name = ':bust_in_silhouette:  Кому', value =f"**{member.mention}**")
-	emb.add_field(name = ':dollar: Сколько', value = f"**{cash}**")
+	emb.add_field(name = 'Кому', value =f"**{member.mention}**")
+	emb.add_field(name = 'Сколько', value = f":bust_in_silhouette: **{cash}**")
 	emb.set_image(url = "https://cdn.discordapp.com/attachments/1093147291327668335/1093250649338167296/unknown_11.png")
 	await ctx.send(embed = emb)
 	await ctx.message.delete()
 	connection.commit()
 
 async def balance_message(ctx, value, value1, value2):
-	emb = discord.Embed(title = 'Баланс', color = 0xffb7fb)
+	emb = discord.Embed(title = 'Баланс', color = 0xB9FFA8)
 	emb.set_author(name = value1, icon_url = value2)
 	local_cash = f"""{cursor.execute("SELECT cash FROM users WHERE id = {}".format(value)).fetchone()[0]}"""
 	local_bank = f"""{cursor.execute("SELECT bank FROM users WHERE id = {}".format(value)).fetchone()[0]}"""
 	local_cash = int(local_cash)
 	local_bank = int(local_bank)
-	emb.add_field(name = ':dollar: Наличные', value = f"**{local_cash}**")
-	emb.add_field(name = ':bank: В банке', value = f"**{local_bank}**")
-	emb.add_field(name = ':credit_card: В общем', value = f"**{local_cash + local_bank}**")
+	emb.add_field(name = 'Наличные', value = f"**{local_cash}** :coin: ")
+	emb.add_field(name = 'В банке', value = f"**{local_bank}** :coin: ")
+	emb.add_field(name = 'В общем', value = f"**{local_cash + local_bank}** :coin: ")
+	emb.set_image(url = "https://cdn.discordapp.com/attachments/1093147291327668335/1093250649338167296/unknown_11.png")
+	await ctx.send(embed = emb)
+	await ctx.message.delete()
+	connection.commit()
+
+async def deposit_message(ctx, value, value1):
+	emb = discord.Embed(title = 'Депозит', color = 0xB9FFA8)
+	emb.set_author(name = ctx.author.name, icon_url = ctx.author.avatar)
+	emb.add_field(name = 'Положил', value = f":bust_in_silhouette: **{value}**")
+	emb.add_field(name = 'Комиссия 5%', value = f":bust_in_silhouette: **{value1}**")
 	emb.set_image(url = "https://cdn.discordapp.com/attachments/1093147291327668335/1093250649338167296/unknown_11.png")
 	await ctx.send(embed = emb)
 	await ctx.message.delete()
@@ -59,59 +69,79 @@ async def balance(ctx, member: discord.Member = None):
 	else:
 		await balance_message(ctx, member.id, member.name, member.avatar)
 
+@client.command()
+async def deposit(ctx, amount: str = None):
+	if amount is None:
+		await false_message(ctx, 'Сумма не была указана')
+	else:
+		if amount == all:
+			local_cash = f"""{cursor.execute("SELECT cash FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]}"""
+			local_cash = int(local_cash)
+			cursor.execute("UPDATE users SET cash = cash - {} WHERE id = {}".format(local_cash, ctx.author.id))
+			cursor.execute("UPDATE users SET bank = bank + {} WHERE id = {}".format(local_cash, ctx.author.id))
+			await deposit_message(ctx, local_cash, local_cash)
+		else:
+			amount = int(amount)
+			if amount <= 0:
+				await false_message(ctx, 'Указанная сумма должна быть больше нуля')
+			else:
+				local_cash = f"""{cursor.execute("SELECT cash FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]}"""
+				local_cash = int(local_cash)
+				commission = amount / 100 * 5
+				commission = int(commission)
+				if amount  > local_cash:
+					await false_message(ctx, 'На балансе недостаточно денег')
+				else:
+					cursor.execute("UPDATE users SET cash = cash - {} WHERE id = {}".format(amount, ctx.author.id))
+					cursor.execute("UPDATE users SET bank = bank + {} WHERE id = {}".format(amount, ctx.author.id))
+					cursor.execute("UPDATE users SET bank = bank - {} WHERE id = {}".format(commission, ctx.author.id))
+					await deposit_message(ctx, amount, commission)
+
+
+
+
+
+
 @client.command(aliases = ['give'])
 async def __give(ctx, member: discord.Member = None, amount: int = None):
 	if member is None:
-		await false_member(ctx, 'Пользователь не был указан')
+		await false_message(ctx, 'Пользователь не был указан')
 	else:
 		if amount is None:
-			await false_member(ctx, 'Сумма была указана неверно')
+			await false_message(ctx, 'Сумма была указана неверно')
 		elif amount < 1:
-			await false_member(ctx, 'Указанная сумма должна быть больше нуля')
+			await false_message(ctx, 'Указанная сумма должна быть больше нуля')
 		else:
 			cursor.execute("UPDATE users SET cash = cash + {} WHERE id = {}".format(amount, member.id))
 			local_cash = cursor.execute("SELECT cash FROM users WHERE id = {}".format(member.id))
-			await true_member(ctx, 'Добавил к балансу', member, local_cash)
+			await true_message(ctx, 'Добавил к балансу', member, local_cash)
 
 @client.command(aliases = ['take'])
 async def __take(ctx, member: discord.Member = None, amount: int = None):
 	if member is None:
-		await false_member(ctx, 'Пользователь не был указан')
+		await false_message(ctx, 'Пользователь не был указан')
 	else:
 		if amount is None:
-			await false_member(ctx, 'Сумма не была указана')
+			await false_message(ctx, 'Сумма не была указана')
 		elif amount < 1:
-			await false_member(ctx, 'Указанная сумма должна быть больше нуля')
+			await false_message(ctx, 'Указанная сумма должна быть больше нуля')
 		else:
 			cursor.execute("UPDATE users SET cash = cash - {} WHERE id = {}".format(amount, member.id))
 			local_cash = cursor.execute("SELECT cash FROM users WHERE id = {}".format(member.id))
-			await true_member(ctx, 'Cнял с баланса', member, local_cash)
+			await true_message(ctx, 'Cнял с баланса', member, local_cash)
 
 @client.command(aliases = ['set'])
 async def __set(ctx, member: discord.Member = None, amount: int = None):
 	if member is None:
-		await false_member(ctx, 'Пользователь не был указан')
+		await false_message(ctx, 'Пользователь не был указан')
 	else:
 		if amount is None:
-			await false_member(ctx, 'Сумма не была указана')
+			await false_message(ctx, 'Сумма не была указана')
 		elif amount < 0:
-			await false_member(ctx, 'Указанная сумма должна быть больше нуля')
+			await false_message(ctx, 'Указанная сумма должна быть больше нуля')
 		else:
 			cursor.execute("UPDATE users SET cash = {} WHERE id = {}".format(amount, member.id))
 			local_cash = f"""{cursor.execute("SELECT cash FROM users WHERE id = {}".format(member.id)).fetchone()[0]}"""
-			await true_member(ctx, 'Установил баланс', member, local_cash)
-
-# @client.command(aliases = ['rep'])
-# async def __rep(ctx, member: discord.Member = None):
-# 	if member is None:
-# 		await false_member(ctx, 'Пользователь не был указан')
-# 	else:
-# 		if member.id == ctx.author.id:
-# 			await false_member(ctx, 'Вы не можете указывать самого себя')
-# 		else:
-# 			cursor.execute("UPDATE users SET rep = rep + {} WHERE id = {}".format(1, member.id))
-# 			connection.commit()
-
-# 			await true_member(ctx, 'Добавил репутацию', member, 1)
+			await true_message(ctx, 'Установил баланс', member, local_cash)
 
 client.run('MTA5MTI5MTcwMjM5ODAyNTc1OQ.GxR3SB.PWy0P8Vp1nsoVnaVfbQrtI66Zx2hg-vVmSxjrA')
